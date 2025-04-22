c09faad662829ffed6f9bf581738ac7f1d847917
Morris <morrisrcodes@gmail.com>
2025-04-22 12:04:42 -0400

    added session logout

--- app.py
+++ app.py
@@ -169,5 +169,11 @@ def report():
     print("Inserted into DB successfully")
     return jsonify({"message": "Report submitted successfully!"})
 
+@app.route("/logout")
+def logout():
+    session.clear()
+    flash("You have been logged out.")
+    return redirect(url_for("login"))
+
 if __name__ == "__main__":
     app.run(debug=True)
